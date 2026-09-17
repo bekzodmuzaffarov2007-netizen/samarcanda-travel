47564c0b20acfefcf833a9846671314022a2e9f2
bekzodmuzaffarov2007-netizen <bekzodmuzaffarov2007@gmail.com>
2026-09-17 22:20:13 +0500

    Add Cesium ion client configuration

---
+++ cesium-config.js
@@ -0,0 +1,3 @@
+// Client-side Cesium configuration for the Samarcanda Travel website.
+// Add your Cesium ion token here. Do not put Supabase service-role keys or other server secrets in this file.
+window.SAMARCANDA_CESIUM_ION_TOKEN = "PASTE_YOUR_CESIUM_ION_TOKEN_HERE";
